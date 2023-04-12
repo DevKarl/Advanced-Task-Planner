7743130ae9aa136ef357e7745abf3c66cb824437
DevKarl <karl-dev@karl-dev.com>
2023-04-12 11:29:46 +0200

    in work: settings modal

--- src/components/Container.js
+++ src/components/Container.js
@@ -6,12 +6,14 @@ import InputField from "./InputField";
 import TaskList from "./TaskList";
 import './UI/CSSvariables.module.css';
 import TaskActions from './TaskActions';
+import Settings from './Settings';
 
 const Container = () => {
 
     return(
         <main className = {classes.container}>
           <TasksContextProvider>
+            <Settings/>
             <Header/>
             <InputField/>
             <TaskActions/>
---
+++ src/components/Settings.js
@@ -0,0 +1,69 @@
+import settings from '../assets/settings.png';
+import Modal from './UI/Modal';
+import { useState } from 'react';
+
+const Settings = props => {
+
+    const [style, setStyle] = useState({
+        backgroundImage: `url(${settings})`,
+        backgroundSize: 'cover',
+        width: '30px',
+        height: '30px',
+        alignSelf: 'end',
+        marginRight: '5px',
+        marginTop: '5px',
+        transition: 'transform 0.3s ease-in-out',
+        transform: 'rotate(0deg)',
+        cursor: 'pointer',
+    });
+
+    const [settingsModalActive, togglesettingsModalActive] = useState(false);
+
+    const handleMouseEnter = () => {
+        setStyle({
+            ...style,
+            transform: 'rotate(90deg)',
+        });
+    };
+
+    const handleMouseLeave = () => {
+        setStyle({
+            ...style,
+            transform: 'rotate(0deg)',
+        });
+    };
+
+    const handleClick = () => {
+        setStyle({
+            ...style,
+            transform: 'rotate(90deg)',
+        });
+        togglesettingsModalActive(prev => !prev);
+    };
+
+    const handleModalMainBtnClick = () => {
+        console.log('clicked settings main btn');
+    }
+
+    return (
+        <>
+        <div
+            style={style}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            onClick={handleClick}
+        ></div>
+        {settingsModalActive && <Modal 
+            closeModalHandler = {togglesettingsModalActive}
+            hasTwoBtns = 'true'
+            btn1Text = 'Save changes'
+            btn2Text = 'Close'
+            mainBtnClick = {handleModalMainBtnClick}
+        />}
+        </>
+    );
+
+};
+
+
+export default Settings;
